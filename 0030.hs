eda68d25fbd2823322f36e034ee82206b84199fc
unknown <060055@A0430-00996.jp.aap.ad.pwcinternal.com>
2014-02-08 00:41:03 +0900

    make 0030.hs

---
+++ 0030.hs
@@ -0,0 +1,16 @@
+
+import qualified Pe
+
+{-
+ - 20.
+ -
+ - n! means n Å~ (n ? 1) Å~ ... Å~ 3 Å~ 2 Å~ 1
+ -
+ - For example, 10! = 10 Å~ 9 Å~ ... Å~ 3 Å~ 2 Å~ 1 = 3628800,
+ - and the sum of the digits in the number 10! is
+ - 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+ -
+ - Find the sum of the digits in the number 100!
+ -}
+
+-- 648
