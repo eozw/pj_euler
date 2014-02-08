79faf363d6c64acfbce55a4fe425f44346bd8ec8
unknown <060055@A0430-00996.jp.aap.ad.pwcinternal.com>
2014-02-08 20:22:39 +0900

    delete Q20. from 0020.hs

--- 0020.hs
+++ 0020.hs
@@ -511,18 +511,3 @@ q8 = q8c . reverse $ q8b
  -}
 
 -- 171
-
-
-{-
- - 20.
- -
- - n! means n Å~ (n ? 1) Å~ ... Å~ 3 Å~ 2 Å~ 1
- -
- - For example, 10! = 10 Å~ 9 Å~ ... Å~ 3 Å~ 2 Å~ 1 = 3628800,
- - and the sum of the digits in the number 10! is
- - 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
- -
- - Find the sum of the digits in the number 100!
- -}
-
--- 648
